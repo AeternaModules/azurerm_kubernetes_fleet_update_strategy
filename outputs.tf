343b0e64380457726c47d7a73f96fb0f2fea28a3
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "kubernetes_fleet_update_strategies" {
   description = "All kubernetes_fleet_update_strategy resources"
   value       = azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies
 }
+output "kubernetes_fleet_update_strategies_kubernetes_fleet_manager_id" {
+  description = "List of kubernetes_fleet_manager_id values across all kubernetes_fleet_update_strategies"
+  value       = [for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : v.kubernetes_fleet_manager_id]
+}
+output "kubernetes_fleet_update_strategies_name" {
+  description = "List of name values across all kubernetes_fleet_update_strategies"
+  value       = [for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : v.name]
+}
+output "kubernetes_fleet_update_strategies_stage" {
+  description = "List of stage values across all kubernetes_fleet_update_strategies"
+  value       = [for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : v.stage]
+}
 
