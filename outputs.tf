64c352a66ff35dfe015b2694c61ae42a0a198936
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "kubernetes_fleet_update_strategies" {
-  description = "All kubernetes_fleet_update_strategy resources"
-  value       = azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies
-}
 output "kubernetes_fleet_update_strategies_kubernetes_fleet_manager_id" {
-  description = "List of kubernetes_fleet_manager_id values across all kubernetes_fleet_update_strategies"
-  value       = [for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : v.kubernetes_fleet_manager_id]
+  description = "Map of kubernetes_fleet_manager_id values across all kubernetes_fleet_update_strategies, keyed the same as var.kubernetes_fleet_update_strategies"
+  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.kubernetes_fleet_manager_id }
 }
 output "kubernetes_fleet_update_strategies_name" {
-  description = "List of name values across all kubernetes_fleet_update_strategies"
-  value       = [for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : v.name]
+  description = "Map of name values across all kubernetes_fleet_update_strategies, keyed the same as var.kubernetes_fleet_update_strategies"
+  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.name }
 }
 output "kubernetes_fleet_update_strategies_stage" {
-  description = "List of stage values across all kubernetes_fleet_update_strategies"
-  value       = [for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : v.stage]
+  description = "Map of stage values across all kubernetes_fleet_update_strategies, keyed the same as var.kubernetes_fleet_update_strategies"
+  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.stage }
 }
 
