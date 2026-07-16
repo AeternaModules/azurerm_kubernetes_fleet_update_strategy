212e465caf5da593d441cb8572c20958857a24a8
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "kubernetes_fleet_update_strategies_id" {
   description = "Map of id values across all kubernetes_fleet_update_strategies, keyed the same as var.kubernetes_fleet_update_strategies"
-  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.id }
+  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "kubernetes_fleet_update_strategies_kubernetes_fleet_manager_id" {
   description = "Map of kubernetes_fleet_manager_id values across all kubernetes_fleet_update_strategies, keyed the same as var.kubernetes_fleet_update_strategies"
-  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.kubernetes_fleet_manager_id }
+  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.kubernetes_fleet_manager_id if v.kubernetes_fleet_manager_id != null && length(v.kubernetes_fleet_manager_id) > 0 }
 }
 output "kubernetes_fleet_update_strategies_name" {
   description = "Map of name values across all kubernetes_fleet_update_strategies, keyed the same as var.kubernetes_fleet_update_strategies"
-  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.name }
+  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "kubernetes_fleet_update_strategies_stage" {
   description = "Map of stage values across all kubernetes_fleet_update_strategies, keyed the same as var.kubernetes_fleet_update_strategies"
-  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.stage }
+  value       = { for k, v in azurerm_kubernetes_fleet_update_strategy.kubernetes_fleet_update_strategies : k => v.stage if v.stage != null && length(v.stage) > 0 }
 }
 
